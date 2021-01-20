2bcad94ccaa6c9b40597961dbc9fdc0422e0559f
Angel Ramos <aramos@techcu.com>
2021-01-20 13:59:07 -0800

    fix

--- NativoPlusStudio.AuthToken.FicosoTests/BaseConfiguration.cs
+++ NativoPlusStudio.AuthToken.FicosoTests/BaseConfiguration.cs
@@ -26,30 +26,30 @@ namespace NativoPlusStudio.AuthToken.FicosoTests
 
             var services = new ServiceCollection();
 
-            services.AddFicosoAuthTokenProvider((options, builder) =>
-            {
-                options.AddFicosoOptions(
-                    protectedResource: configuration["FicosoOptions:ProtectedResourceName"],
-                    accessTokenEndpoint: configuration["FicosoOptions:AccessTokenEndpoint"],
-                    clientId: configuration["FicosoOptions:ClientId"],
-                    clientSecret: configuration["FicosoOptions:ClientSecret"],
-                    grantType: configuration["FicosoOptions:GrantType"],
-                    userName: configuration["FicosoOptions:UserName"],
-                    password: configuration["FicosoOptions:Password"],
-                    scope: configuration["FicosoOptions:Scope"],
-                    url: configuration["FicosoOptions:Url"],
-                    includeEncryptedTokenInResponse: true
-                );
-            });
+            //services.AddFicosoAuthTokenProvider((options, builder) =>
+            //{
+            //    options.AddFicosoOptions(
+            //        protectedResource: configuration["FicosoOptions:ProtectedResourceName"],
+            //        accessTokenEndpoint: configuration["FicosoOptions:AccessTokenEndpoint"],
+            //        clientId: configuration["FicosoOptions:ClientId"],
+            //        clientSecret: configuration["FicosoOptions:ClientSecret"],
+            //        grantType: configuration["FicosoOptions:GrantType"],
+            //        userName: configuration["FicosoOptions:UserName"],
+            //        password: configuration["FicosoOptions:Password"],
+            //        scope: configuration["FicosoOptions:Scope"],
+            //        url: configuration["FicosoOptions:Url"],
+            //        includeEncryptedTokenInResponse: true
+            //    );
+            //});
 
-            services.AddHttpClient<FCSHttpClient>(async (provider, client) =>
-            {
-                client.BaseAddress = new Uri(configuration["FicosoOptions:Url"]);
+            //services.AddHttpClient<FCSHttpClient>(async (provider, client) =>
+            //{
+            //    client.BaseAddress = new Uri(configuration["FicosoOptions:Url"]);
 
-                var token = await provider.GetService<IAuthTokenGenerator>().GetTokenAsync(protectedResource: configuration["FicosoOptions:ProtectedResourceName"]);
-                client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue(scheme: "Bearer", parameter: token.Token);
-            })
-            .AddRefreshFicosoTokenPolicyWithJitteredBackoff(protectedResourceName: configuration["FicosoOptions:ProtectedResourceName"], initialDelayInSeconds: 1, retryCount: 2)
+            //    var token = await provider.GetService<IAuthTokenGenerator>().GetTokenAsync(protectedResource: configuration["FicosoOptions:ProtectedResourceName"]);
+            //    client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue(scheme: "Bearer", parameter: token.Token);
+            //})
+            //.AddRefreshFicosoTokenPolicyWithJitteredBackoff(protectedResourceName: configuration["FicosoOptions:ProtectedResourceName"], initialDelayInSeconds: 1, retryCount: 2)
             //.AddRefreshFicosoTokenPolicyWithConstantBackoff(protectedResourceName: configuration["FicosoOptions:ProtectedResourceName"], initialDelayInSeconds: 1, retryCount: 2)
             //.AddRefreshFicosoTokenPolicyWithExponentialBackoff(protectedResourceName: configuration["FicosoOptions:ProtectedResourceName"], initialDelayInSeconds: 1, retryCount: 2)
             //.AddRefreshFicosoTokenPolicyWithLinearBackoff(protectedResourceName: configuration["FicosoOptions:ProtectedResourceName"], initialDelayInSeconds: 1, retryCount: 2)
